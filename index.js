5653bafc2195ae6ff90d17248ad8e57b53f2f3a9
Tony Eneh <aeneh90@gmail.com>
2019-08-02 16:32:36 +0100

    Serve file contents

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // import * as http from 'http';
 const http = require('http');
-const fs = require('filesystem');
+const fs = require('fs');
 const url = require('url');
 
 const server = http.createServer((req, res)=>{ 
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('got your request niggi');
-    res.end();
+    res.write('got your request niggi<br/>');
+    fs.readFile('some-file', (err, contents)=>{
+        if(err){
+            console.log('file reading error', err)
+            res.end();
+        }else{
+            res.write('See contents of the file below:<br/>');
+            res.write(contents);
+            res.end();
+        }
+    });
 });
 server.listen(3000, (console.log('listening at port 3000')));
