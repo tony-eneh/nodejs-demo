b0882562cc3b1fd984b89e65fd10428598f7a55c
Tony Eneh <aeneh90@gmail.com>
2019-08-06 20:04:32 +0100

    Create mysql table

--- db-setup.js
+++ db-setup.js
@@ -3,14 +3,17 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Uchenna90"
+    password: "Uchenna90",
+    database: "mydb"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function(err, result) {
+    const sql = "CREATE TABLE student_records (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)";
+    con.query(sql, function(err, result) {
         if (err) throw err;
-        console.log("Database created");
+        console.log("Table was successfully created");
+        console.log(result);
     });
 });
