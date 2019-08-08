694cfcca24c12f513c87b2861ffd8e9a0958db7e
Tony Eneh <aeneh90@gmail.com>
2019-08-08 15:14:11 +0100

    Create collection in db

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-const url = "mongodb://localhost:27017/mydb";
+const url = "mongodb://localhost:27017/";
 
 //mongo creates the db if it doesnt already exist
 MongoClient.connect(url, function(err, db) {
+    const dbo = db.db('mydb');
     if (err) throw err;
     console.log("Database created!");
-    db.close();
+    dbo.createCollection('student_records', (err, collection) => {
+        if (err) throw err;
+        console.log("Collection created", collection);
+        db.close();
+    });
+
 });
