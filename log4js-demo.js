e95477e1b79f0f8c175be9e5c1c2b2adac660e33
Tony Eneh <aeneh90@gmail.com>
2019-08-28 05:44:30 +0100

    Use every inbuilt logging level

--- log4js-demo.js
+++ log4js-demo.js
@@ -1,4 +1,15 @@
-const logger = require('log4js').getLogger();
+const log4js = require('log4js');
 
-logger.level = 'debug';
+log4js.configure({
+    appenders: { ewoo: { type: "file", filename: "ewoo.log" } },
+    categories: { default: { appenders: ['ewoo'], level: "trace" } }
+})
+
+const logger = log4js.getLogger();
+logger.trace("this is a trace message");
 logger.debug("Some debug messages");
+logger.info("info message");
+logger.warn("be warned");
+logger.error("an error has occurred");
+logger.fatal("this is a fatal error!");
+logger.mark("this is the 'mark' logging level");
