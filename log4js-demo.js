c972ae9271d67ea444f694ae4093f48562f2e1db
Tony Eneh <aeneh90@gmail.com>
2019-08-28 05:56:45 +0100

    Try to commit custom log level

--- log4js-demo.js
+++ log4js-demo.js
@@ -1,6 +1,7 @@
 const log4js = require('log4js');
 
 log4js.configure({
+    levels: { "nsibull": { value: 10, color: "green" } },
     appenders: { ewoo: { type: "file", filename: "ewoo.log" } },
     categories: { default: { appenders: ['ewoo'], level: "trace" } }
 })
@@ -13,3 +14,4 @@ logger.warn("be warned");
 logger.error("an error has occurred");
 logger.fatal("this is a fatal error!");
 logger.mark("this is the 'mark' logging level");
+logger.nsibull("this is a custom logging level called nsibull");
