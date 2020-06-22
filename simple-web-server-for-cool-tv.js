f01fa0a840316ae35cc124679b5dda6b717d7333
Tony Eneh <aeneh90@gmail.com>
2020-06-22 11:11:31 +0100

    feat: setup a simple web server for cool tv api

---
+++ simple-web-server-for-cool-tv.js
@@ -0,0 +1,149 @@
+var http = require("http");
+
+const model = {
+  // TODO: Check the exact nature of m3u8 files and find out
+  // whether they support single url for stream OR each URL show an
+  // interactive UI just like a DVD.
+
+  channels: [
+    {
+      id: "jgoi49sk894uhk933h94hg1",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg2",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg3",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg4",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg5",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg6",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg7",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg8",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg9",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg10",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg11",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg12",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg13",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg14",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+    {
+      id: "jgoi49sk894uhk933h94hg15",
+      url: "https://ewoo.com/egwu-diya.m3u8",
+      name: "Egwu Diya!",
+      description:
+        "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
+    },
+  ],
+
+  getChannels: () => {
+    return JSON.stringify(model.channels);
+  },
+
+  getChannel: (id) => {
+    const data = model.channels.filter((channel) => channel.id === id);
+    return JSON.stringify(data);
+  },
+};
+
+http
+  .createServer(function (req, res) {
+    // A parsed url to work with in case there are parameters
+    var _url;
+    // In case the client uses lower case for methods.
+    req.method = req.method.toUpperCase();
+    console.log(req.method + " " + req.url);
+
+    // allow cors requests
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    if ((_url = /^\/api\/channels$/i.exec(req.url))) {
+      // return a list of channels
+      res.writeHead(200);
+      return res.end(model.getChannels());
+    } else if ((_url = /^\/api\/channels\/(\[A-Za-z0-9]+)$/i.exec(req.url))) {
+      // find the channel by the id in the route
+      res.writeHead(200);
+      return res.end(model.getChannel(id));
+    } else {
+      res.end("couldn't fetch no sh*t");
+    }
+  })
+  .listen(process.env.PORT || 4200);
