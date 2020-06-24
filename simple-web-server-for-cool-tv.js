f887f81726d663a7cebe76e2b994564b16ea31b4
Tony Eneh <aeneh90@gmail.com>
2020-06-24 23:03:20 +0100

    feat: alter names for data

--- simple-web-server-for-cool-tv.js
+++ simple-web-server-for-cool-tv.js
@@ -8,85 +8,85 @@ const model = {
   channels: [
     {
       id: "jgoi49sk894uhk933h94hg1",
-      url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      url: "http://45.126.83.51/uq2663/h/h03/index.m3u8",
+      name: "Bloomberg TV Asia (HK)!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg2",
-      url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      url: "http://103.199.161.254/Content/bbcworld/Live/Channel(BBCworld)/index.m3u8",
+      name: "BBC World News!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg3",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Nwa nigga!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg4",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Heyoo!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg5",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Dats we roh!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg6",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Enweee!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg7",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "O nweee buru ya!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg8",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Chineke Umunwanyi eeh!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg9",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Naso!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg10",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Looks at you!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg11",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Plans and bensicle!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
     {
       id: "jgoi49sk894uhk933h94hg12",
       url: "https://ewoo.com/egwu-diya.m3u8",
-      name: "Egwu Diya!",
+      name: "Aru na aru!",
       description:
         "ipsum lorem est in hoc nihi et haca at deoun git ata arakapatune egosum echial mongoose mongo node express hapi mysql in suru suru ewooo chai.",
     },
@@ -119,6 +119,7 @@ const model = {
 
   getChannel: (id) => {
     const data = model.channels.filter((channel) => channel.id === id);
+    console.log("data from model:", data);
     return JSON.stringify(data);
   },
 };
@@ -138,9 +139,12 @@ http
       // return a list of channels
       res.writeHead(200);
       return res.end(model.getChannels());
-    } else if ((_url = /^\/api\/channels\/(\[A-Za-z0-9]+)$/i.exec(req.url))) {
+    } else if ((_url = /^\/api\/channels\/([A-Za-z0-9]+)$/i.exec(req.url))) {
       // find the channel by the id in the route
+      console.log("got channel shit");
+      console.log(_url);
       res.writeHead(200);
+      let id = _url[1]; /** the second item in the array is my desired id */
       return res.end(model.getChannel(id));
     } else {
       res.end("couldn't fetch no sh*t");
