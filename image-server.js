e98806346291d464422ac52cca3ec59b5bb65f45
Tony Eneh <aeneh90@gmail.com>
2020-11-02 18:38:34 +0100

    Feat: user can request a specific no of urls

--- image-server.js
+++ image-server.js
@@ -1,3 +1,22 @@
+/**
+ * an express API for image urls
+ *
+ * How to use
+ * GET /images?n=no_of_images
+ *
+ * send a get request to the above url where the query parameter n should
+ * be a number representing the no of images you want
+ *
+ * Example: to fetch 100 random images
+ *
+ * GET /images?n=100
+ *
+ * Sending a request without n query parameter results in a response
+ * with an array containing 18 urls. This is the default.
+ *
+ *
+ */
+
 const app = require("express")();
 
 console.log("image-server page actually ran");
@@ -19,7 +38,6 @@ const images = [
   "https://homepages.cae.wisc.edu/~ece533/images/frymire.png",
   "https://homepages.cae.wisc.edu/~ece533/images/girl.png",
   "https://homepages.cae.wisc.edu/~ece533/images/goldhill.png",
-  "https://homepages.cae.wisc.edu/~ece533/images/lenacolor.png",
   "https://homepages.cae.wisc.edu/~ece533/images/monarch.png",
   "https://homepages.cae.wisc.edu/~ece533/images/mountain.png",
   "https://homepages.cae.wisc.edu/~ece533/images/peppers.png",
@@ -31,9 +49,20 @@ const images = [
   "https://homepages.cae.wisc.edu/~ece533/images/zelda.png",
 ];
 
+const selectRandomImage = () =>
+  images[Math.floor(Math.random() * images.length)];
+
 app.get("/images", (req, res) => {
   console.log("image was actually fetched");
-  res.status(200).send(images);
+  console.log(req.query);
+
+  // if number of images is specified, create an array with such number of urls.
+  // If not, just return your predefined array of image urls
+  const data = req.query.n
+    ? Array.from({ length: req.query.n }).map(selectRandomImage)
+    : images;
+
+  res.status(200).send(data);
 });
 
 app.listen(process.env.PORT || 3000);
